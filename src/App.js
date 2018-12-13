781512cf41fa10a2d2686dc00660d60ee0ea9832
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-12-13 09:56:29 +0100

    obym zdał xd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api?results=10")
+      .then(response => response.json())
+      .then(data => this.setState({ data: data.results }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.data &&
+          this.state.data.map(user => (
+            <div>
+              <h1> Wielki Finał !</h1>
+              <h2> prosze o zaliczenie xd </h2>
+              <div>
+                <img src={user.picture.medium}
+                />
+              </div>
+              <div>
+                {user.email}
+              </div>
+              <div>
+                {user.name.last}
+                {user.name.first}
+              </div>
+              <hr />
+            </div>
+          )
+          )
+        }
       </div>
     );
   }
